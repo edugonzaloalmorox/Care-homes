4c21bc02e1e47dc468a7c4e48030ff864764b48a
edugonzaloalmorox <eduardogonzaloalmorox@gmail.com>
2016-09-27 14:54:05 +0100

    CQC data management

---
+++ R/cqc_data.R
@@ -0,0 +1,8 @@
+# importing CQC data and cleaning some variables 
+
+# created: 27/09/2016
+# modified: 27/09/2016
+# author: Edu Gonzalo (Newcastle University)
+# ------------------------------------------
+
+# This script contains the 
