07e3ce145745e7c8370f3fd8e20c8de75cb12861
edugonzaloalmorox <eduardogonzaloalmorox@gmail.com>
2016-09-29 13:51:37 +0100

    New functions

--- R/funciones .R
+++ R/funciones .R
@@ -2,7 +2,6 @@
 # --------------------------
 
 
-
 # rename variables of the dataset 
       rename.var <- function(dta){
         
@@ -14,10 +13,27 @@
       }
 
 
-#clean care homes names
+# clean care homes names
       clean <- function(dta){
         dta = gsub("[[:digit:]]+", "", dta)
         dta = str_trim(dta, side = "both")
+        dta = gsub(".*-","",dta)
+        dta = str_replace_all(dta, "[[:punct:]]", "")
         return(dta)
       }
 
+
+# recoding missing information associated with variables 
+      
+      recode.missing <- function(dta, g1, g2, a){
+        # dta is data frame
+        # g1 : grouping variable 1
+        # g2 : grouping variable 2 
+        # a is variabe
+      dta = dta %>%
+        group_by(g1, g2) %>%
+        mutate(a=na.omit(a)[1])
+      
+      return(dta)
+      
+      }
--- funciones .R
+++
@@ -1,9 +0,0 @@
-# load data 
-
-#clean variables
-clean <- function(dta){
-  dta = gsub("[[:digit:]]+", "", dta)
-  dta = str_trim(dta, side = "both")
-  return(dta)
-}
-
