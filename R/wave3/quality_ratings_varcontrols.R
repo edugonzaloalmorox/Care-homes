32101d0caaca7788ab00b06bf061218e89de8f98
edugonzaloalmorox <eduardogonzaloalmorox@gmail.com>
2017-01-24 11:49:56 +0000

    Sample creation

--- R/wave3/quality_ratings_varcontrols.R
+++ R/wave3/quality_ratings_varcontrols.R
@@ -2,6 +2,11 @@
 # Created 23/01/2016
 # Modified 24/01/2016
 
+# ---------------------------------------------
+# Building the sample for alternative outcomes 
+# ---------------------------------------------
+
+
 
  benefits_year_district = import("/Users/Personas/My Cloud/PhD _october_2016/market entry/care_homes/data/processed/benefits_year_district.csv")
  
@@ -19,10 +24,9 @@
  write.csv(benefits, "/Users/Personas/My Cloud/PhD _october_2016/market entry/care_homes/data/waves/three/benefits.csv", row.names = FALSE)  
  
  
- 
- 
- # link count ratings with benefits
- 
+# -------------------------------- 
+# link count ratings with benefits
+# --------------------------------
        write.csv(count_ratings, "/Users/Personas/My Cloud/PhD _october_2016/market entry/care_homes/data/waves/three/countratings_wide.csv", row.names = FALSE)  
        
        
@@ -54,8 +58,11 @@
        
        test = left_join(prueba.w, benefits, by = c("oslaua" = "la_code", "wave" = "wave"))
        # there are less observations in test because there are some local authorities that were not inspected during the first wave
- 
+
+       
+# ------------ 
 # house prices 
+# ------------
        
        years = c(2014, 2015, 2016)
        
@@ -64,8 +71,46 @@
        av.prices = unique(av.prices)
        av.prices  = av.prices %>% filter(!grepl("W0", oslaua)) %>% filter(!grepl("S1", oslaua)) # drop observations from Scotland and Wales
        
-# link house prices with benefits and ratings 
+       av.prices = av.prices %>% mutate(wave = ifelse(year.trans == 2014, 1,
+                                                      ifelse(year.trans == 2015, 2,
+                                                      ifelse(year.trans == 2016, 3, "other")))) %>% select(-year.trans)
+
+# -------------------------------------------       
+#link house prices with benefits and ratings
+# --------------------------------------------
+       
+       av.prices$wave = as.numeric(av.prices$wave)
+       test$wave = as.numeric(test$wave)
+       
+       test.1 = left_join(test, av.prices, by = c("oslaua", "wave"))
+       
+       # select information from the parent sample (test_3)
+       
+      redtest3 = test_3 %>% select(wave:dclg.code, cum_homes, total_pop:trafsqtrend, hhi, people_old65, local.authority)
+      
+      
+      # link, order and clean redundant variables
+      # ------------------------------------------
+      
+      test.2 = left_join(test.1, redtest3, by = c("oslaua", "wave")) %>% select(wave, oslaua, lpa = lpa.x, -lpa.y, la:dclg.code, Location.Region, local.authority, Bad:people_old65)
+       
+      # create variables of interest
+      
+      test.2 = test.2 %>% mutate_each(funs(as.numeric), total_pop, total_old, jsa_fem, carers_allowance, pension_credits, income_support) %>%
+                                      mutate(prop.old.la = (total_old/total_pop),
+                                 prop.jsa.la = (jsa_fem/total_pop),
+                                 prop.allowance.la = (carers_allowance/total_pop),
+                                 prop.pension.la = (pension_credits/total_pop),
+                                 prop.income.sup.la = (income_support/total_pop))
+       
+      test.2 = test.2 %>% mutate(Outstanding = ifelse(is.na(Outstanding), 0, Outstanding))
+      
+      # proportion rated care homes 
+      
+      test.2 = test.2 %>% mutate(prop.good = (Good/cum_homes), 
+                                 prop.bad = (Bad/cum_homes),
+                                 prop.outstanding = (Outstanding/cum_homes)) %>% select(-date)
        
        
-       
+      write.csv(test.2, "/Users/Personas/My Cloud/PhD _october_2016/market entry/care_homes/data/waves/three/test_4.csv", row.names = FALSE)
       
